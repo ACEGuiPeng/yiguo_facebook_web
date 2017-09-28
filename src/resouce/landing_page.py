a0d6e54cd1f974628229bcf5c7d8aefb669d3b3e
guipeng <guipeng8789502@163.com>
2017-09-28 20:57:31 +0800

    commit 20170928

--- src/resouce/landing_page.py
+++ src/resouce/landing_page.py
@@ -4,9 +4,14 @@ from flask import request, render_template, make_response, Response
 
 from flask_restful import Resource
 
+mapper = {
+    'html': 'text/html',
+    'jpeg': "image/jpeg",
+    # ...
+}
+
 
 class LandingPage(Resource):
-    decorators = {}
     '''
     http://127.0.0.1:5000/ygfbad/web/land_page
     {
@@ -19,16 +24,12 @@ class LandingPage(Resource):
     def get(self, product_name, time, order_num, file_name):
 
         if isinstance(file_name, str):
-            if file_name.endswith('html'):
-                response = make_response(render_template('{}/{}/{}/{}'.format(product_name, time, order_num, file_name),
-                                                         order_num=order_num))
-            else:
-                file = os.path.abspath('./templates/{}/{}/{}/{}'.format(product_name, time, order_num, file_name))
-                image = open(file, 'rb')
-                response = Response(image, mimetype="image/jpeg")
-
-            return response
+            file_path = os.path.abspath('./templates/{}/{}/{}/{}'.format(product_name, time, order_num, file_name))
+            for suffix, mimetype in mapper.items():
+                if file_name.endswith(suffix):
+                    file = open(file_path, 'rb')
+                    response = Response(file, mimetype=mimetype)
+                    return response
 
     def post(self, product_name, time, order_num, file_name):
-
         return self.get(product_name, time, order_num, file_name)
